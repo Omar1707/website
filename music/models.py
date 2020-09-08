7b4ba14592c70a73e90640182680f3cdbb0630d6
Omar Yasser <omar1707@gmail.com>
2020-09-08 11:52:35 +0200

    9/7/2020

--- music/models.py
+++ music/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Album(models.Model):
     artist = models.CharField(max_length=250) 
     title = models.CharField(max_length=250)
     logo = models.CharField(max_length=250)
     genre = models.CharField(max_length=250)
-
+    
+    def get_absolute_url(self):
+        return reverse("music:detail", kwargs={"pk": self.pk})
+    
     def __str__(self):
         return self.title + " - " + self.artist
 
--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,9 @@ app_name = "music"
 
 urlpatterns = [
     #/music/
-    path('',views.index,name='index'),
-
+    path('',views.IndexView.as_view(),name='index'),
     #/music/<album_id>/
-    path('<int:album_id>/',views.detail,name='detail'),
-
-    #/music/<album_id>/favorite/
-    path('<int:album_id>/favorite',views.favorite,name='favorite'),
+    path('<int:pk>/',views.DetailView.as_view(),name='detail'),
+    #/music/album/add/
+    path('album/add/',views.AlbumCreate.as_view(),name='album-add'),
 ]
